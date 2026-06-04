0af26a1220b25a14d3c805e55dc57f0e3c04d174
Leandro Alves Carvalho <leandroalvescarvalho@yahoo.com>
2026-06-04 16:46:29 -0300

    quasar adicionado

--- universo.py
+++ universo.py
@@ -2,6 +2,6 @@ class universo:
     def __init__(self):
         self.luz = "Estrelas"
         self.vacuo = "Materia Escura"
-        self.astros = ["Planeta", "Lua", "Sol", "Buraco Negro"]
+        self.astros = ["Planeta", "Lua", "Sol", "Buraco Negro", "Quasar"]
         self.seres = ["Aliens", "Humanos", "Fantasmas", "Marcianos"]
 
